c59acd4bdf9b2e4b7f33c56610f368cd4a8a5ac3
ToXer04 <84339271+ToXer04@users.noreply.github.com>
2024-03-04 16:43:03 +0100

    Update Class.java

--- webapp/src/main/java/hibernate/example/webapp/entities/Class.java
+++ webapp/src/main/java/hibernate/example/webapp/entities/Class.java
@@ -4,9 +4,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.ArrayList;
 
 @Entity
-@Table(name = "classes")
+@Table
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
